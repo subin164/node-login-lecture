067f19c626853861b3e31ab74efa11d9dfb3ab84
subin164 <qkrtnqls5669@gmail.com>
2022-11-06 17:26:51 +0900

    fix : Optimize folder structure

---
+++ app/app.js
@@ -0,0 +1,44 @@
+"use strict";
+
+//모듛
+const express = require("express");
+const app = express();
+
+
+//앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+//라우팅
+const home = require("./src/routes/home");
+
+app.use("/", home);//->//미들웨어 등록하는 방법
+
+
+// app.get("/",(req, res) => {
+//    res.render("home/index");
+// });
+
+// app.get("/login", (req, res) => {
+//    res.render("home/login");
+// });
+// app.listen(PORT, function(){
+//     console.log("서버 가동");
+// });
+
+// const http = require("http");
+// const app = http.createServer((req, res) => {
+//     // console.log(req.url);
+//     res.writeHead(200, {"Content-Type" : "text/html; charset=utf-8" });
+//     if(req.url === "/") {
+//         res.end("여기는 루트입니다.");
+//     }else if(req.url ==="/login"){
+//         res.end("여기는 로그인화면입니다.");
+//     }
+// });
+
+// app.listen(3001, () => {
+//    console.log("http 가동된 서버입니다."); 
+// });
+
+module.exports = app;
