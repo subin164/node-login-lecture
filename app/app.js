d011e4bc4cfd52500c3db3208aa3fcae71355f6d
subin164 <qkrtnqls5669@gmail.com>
2022-11-07 13:57:02 +0900

    front JS file

--- app/app.js
+++ app/app.js
@@ -4,15 +4,16 @@
 const express = require("express");
 const app = express();
 
+//라우팅
+const home = require("./src/routes/home");
 
 //앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
-
-//라우팅
-const home = require("./src/routes/home");
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home);//->//미들웨어 등록하는 방법
+
 
 
 // app.get("/",(req, res) => {
